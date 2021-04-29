5528d459e0e2096b78238d416fed7e1292862b5f
marty <morez.martin@gmail.com>
2021-04-29 09:16:41 +0000

    change logo and app name

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from frappe import _
 
 app_name = "erpnext"
-app_title = "ERPNext"
+app_title = "MHR"
 app_publisher = "Frappe Technologies Pvt. Ltd."
 app_description = """ERP made simple"""
 app_icon = "fa fa-th"
