8e36eed439613b4aeccdee49f33d2241fa7b9022
Debian <debian@packer-output-5e31c8bd-359d-4be3-b5bf-2c9a1e176601>
2021-11-27 13:11:38 +0000

    syntax change

--- erpnext/e_commerce/shopping_cart/cart.py
+++ erpnext/e_commerce/shopping_cart/cart.py
@@ -63,8 +63,7 @@ def get_cart_quotation(doc=None):
 		"terms": get_terms_and_conditions(tc_name),
 		"payment_terms_template": payment_terms_template,
 		"payment_schedule": [],
-		"click_n_collect_addresses": [{"name": address.name, "title": address.address_title, "display": address.display}
-			for address in click_n_collect_addresses],
+		"click_n_collect_addresses": [{"name": address.name, "title": address.address_title, "display": address.display} for address in click_n_collect_addresses],
 		"cart_settings": frappe.get_cached_doc("E Commerce Settings")
 	}
 
