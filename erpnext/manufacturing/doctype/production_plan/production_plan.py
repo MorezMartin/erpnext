34a1ddb8f36b3b7d21c3eedb661986be1e3066f8
Debian <debian@packer-output-5e31c8bd-359d-4be3-b5bf-2c9a1e176601>
2021-11-28 22:01:27 +0000

    fix error on production plan

--- erpnext/manufacturing/doctype/production_plan/production_plan.py
+++ erpnext/manufacturing/doctype/production_plan/production_plan.py
@@ -750,29 +750,29 @@ def get_material_request_items(row, sales_order, company,
 
 def get_sales_orders(self):
 	so_filter = item_filter = ""
-	if self.from_date:
-		so_filter += " and so.transaction_date >= %(from_date)s"
-	if self.to_date:
-		so_filter += " and so.transaction_date <= %(to_date)s"
-	if self.customer:
-		so_filter += " and so.customer = %(customer)s"
-	if self.project:
-		so_filter += " and so.project = %(project)s"
-	if self.sales_order_status:
-		so_filter += "and so.status = %(sales_order_status)s"
+	bom_item = "bom.item = so_item.item_code"
 
-	if self.item_code:
-		item_filter += " and so_item.item_code = %(item)s"
+	date_field_mapper = {
+		'from_date': ('>=', 'so.transaction_date'),
+		'to_date': ('<=', 'so.transaction_date'),
+		'from_delivery_date': ('>=', 'so_item.delivery_date'),
+		'to_delivery_date': ('<=', 'so_item.delivery_date')
+	}
 
+	for field, value in date_field_mapper.items():
+		if self.get(field):
+			so_filter += f" and {value[1]} {value[0]} %({field})s"
 
-	if self.from_delivery_date:
-		so_filter += " and so.delivery_date >= %(from_delivery_date)s"
+	for field in ['customer', 'project', 'sales_order_status']:
+		if self.get(field):
+			so_field = 'status' if field == 'sales_order_status' else field
+			so_filter += f" and so.{so_field} = %({field})s"
 
-	if self.to_delivery_date:
-		so_filter += " and so.delivery_date <= %(to_delivery_date)s"
+	if self.item_code and frappe.db.exists('Item', self.item_code):
+		bom_item = self.get_bom_item() or bom_item
+		item_filter += " and so_item.item_code = %(item_code)s"
 
-
-	open_so = frappe.db.sql("""
+	open_so= frappe.db.sql("""
  		select distinct so.name, so.transaction_date, so.customer, so.base_grand_total, so.delivery_date, so.shipping_address_name
 		from `tabSales Order` so, `tabSales Order Item` so_item
 		where so_item.parent = so.name
@@ -785,17 +785,8 @@ def get_sales_orders(self):
 					where pi.parent = so.name and pi.parent_item = so_item.item_code
 						and exists (select name from `tabBOM` bom where bom.item=pi.item_code
 							and bom.is_active = 1)))
-		""".format(so_filter, item_filter), {
-			"from_date": self.from_date,
-			"to_date": self.to_date,
-			"customer": self.customer,
-			"project": self.project,
-			"item": self.item_code,
-			"company": self.company,
-			"from_delivery_date": self.from_delivery_date,
-			"to_delivery_date": self.to_delivery_date,
-			"sales_order_status": self.sales_order_status
-		}, as_dict=1)
+		""", self.as_dict(), as_dict=1)
+
 	return open_so
 
 @frappe.whitelist()
