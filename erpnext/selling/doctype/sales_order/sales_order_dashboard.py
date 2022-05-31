63b209c71f852f618c60ba9011056cccf7e52d7d
root <morez.martin@gmail.com>
2022-05-31 08:54:01 +0000

    customize sales order dashboard

--- erpnext/selling/doctype/sales_order/sales_order_dashboard.py
+++ erpnext/selling/doctype/sales_order/sales_order_dashboard.py
@@ -11,8 +11,11 @@ def get_data():
 			"Payment Request": "reference_name",
 			"Auto Repeat": "reference_document",
 			"Maintenance Visit": "prevdoc_docname",
+			"Timesheet": "time_logs",
 		},
-		"internal_links": {"Quotation": ["items", "prevdoc_docname"]},
+		"internal_links": {
+            "Quotation": ["items", "prevdoc_docname"]
+            },
 		"transactions": [
 			{
 				"label": _("Fulfillment"),
@@ -23,5 +26,6 @@ def get_data():
 			{"label": _("Manufacturing"), "items": ["Work Order"]},
 			{"label": _("Reference"), "items": ["Quotation", "Auto Repeat"]},
 			{"label": _("Payment"), "items": ["Payment Entry", "Payment Request", "Journal Entry"]},
+			{"label": _("HR"), "items": ["Timesheet"]},
 		],
 	}
