d2597a98d4b45aa9f00043b76267edd6ca3f1002
Martin <morez.martin@gmail.com>
2022-11-10 17:34:24 +0100

    fix conflicts

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -473,51 +473,6 @@ payment_gateway_enabled = "erpnext.accounts.utils.create_payment_gateway_account
 
 communication_doctypes = ["Customer", "Supplier"]
 
-<<<<<<< HEAD
-accounting_dimension_doctypes = [
-	"GL Entry",
-	"Sales Invoice",
-	"Purchase Invoice",
-	"Payment Entry",
-	"Asset",
-	"Expense Claim",
-	"Expense Claim Detail",
-	"Expense Taxes and Charges",
-	"Stock Entry",
-	"Budget",
-	"Payroll Entry",
-	"Delivery Note",
-	"Sales Invoice Item",
-	"Purchase Invoice Item",
-	"Purchase Order Item",
-	"Journal Entry Account",
-	"Material Request Item",
-	"Delivery Note Item",
-	"Purchase Receipt Item",
-	"Stock Entry Detail",
-	"Payment Entry Deduction",
-	"Sales Taxes and Charges",
-	"Purchase Taxes and Charges",
-	"Shipping Rule",
-	"Landed Cost Item",
-	"Asset Value Adjustment",
-	"Loyalty Program",
-	"Fee Schedule",
-	"Fee Structure",
-	"Stock Reconciliation",
-	"Travel Request",
-	"Fees",
-	"POS Profile",
-	"Opening Invoice Creation Tool",
-	"Opening Invoice Creation Tool Item",
-	"Subscription",
-	"Subscription Plan",
-	"POS Invoice",
-	"POS Invoice Item",
-	"Purchase Order",
-	"Purchase Receipt",
-	"Sales Order",
-=======
 advance_payment_doctypes = ["Sales Order", "Purchase Order"]
 
 invoice_doctypes = ["Sales Invoice", "Purchase Invoice"]
@@ -529,7 +484,6 @@ period_closing_doctypes = [
 	"Bank Clearance",
 	"Asset",
 	"Stock Entry",
->>>>>>> 3967773fbeddfd05b53f722f1b51ea813a57b3c1
 ]
 
 bank_reconciliation_doctypes = [
