fb19fc36bd09878296c167b3afeab5bc616d635d
Morez Martin <morez.martin@gmail.com>
2023-01-27 11:12:57 +0100

    add payment_id for payment gateways

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -1068,7 +1068,6 @@ erpnext.TransactionController = class TransactionController extends erpnext.taxe
 			});
 		}
 		me.calculate_stock_uom_rate(doc, cdt, cdn);
-		me.apply_pricing_rule(item, true);
 	}
 
 	conversion_factor(doc, cdt, cdn, dont_fetch_price_list_rate) {
