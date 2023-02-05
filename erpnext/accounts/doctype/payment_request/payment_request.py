66bf797ce72ab052f02d135247de8221df819049
Morez Martin <morez.martin@gmail.com>
2023-02-05 13:33:56 +0100

    test in enhance payment intent confirmation

--- erpnext/accounts/doctype/payment_request/payment_request.py
+++ erpnext/accounts/doctype/payment_request/payment_request.py
@@ -214,8 +214,8 @@ class PaymentRequest(Document):
 			self.db_set("status", "Paid")
 
 		else:
-			self.make_invoice()
 			payment_entry = self.create_payment_entry()
+			self.make_invoice()
 
 			return payment_entry
 
