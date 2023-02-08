eb536037aab28b1ddaf0f5acddc5855c04201f44
Morez Martin <morez.martin@gmail.com>
2023-02-08 16:06:32 +0100

    update make_payment_request

--- erpnext/accounts/doctype/payment_request/payment_request.py
+++ erpnext/accounts/doctype/payment_request/payment_request.py
@@ -416,8 +416,9 @@ def make_payment_request(**args):
 		"Payment Request",
 		{"reference_doctype": args.dt, "reference_name": args.dn, "docstatus": 1},
 	)
+	existing_payment_request_status = frappe.get_value("Payment Request", existing_payment_request, status)
 
-	if existing_payment_request and existing_payment_request.status != 'Paid':
+	if existing_payment_request and existing_payment_request_status != 'Paid':
 		frappe.db.set_value(
 			"Payment Request", existing_payment_request, "grand_total", grand_total, update_modified=False
 		)
