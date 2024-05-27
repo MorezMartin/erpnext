547842e91d509989c2095f1c03160a99a8d789c6
marty <you@example.com>
2024-05-27 13:01:12 +0000

    fix persissions error in Sales Order Timesheet Detail

--- erpnext/selling/doctype/sales_order_timesheet_detail/sales_order_timesheet_detail.py
+++ erpnext/selling/doctype/sales_order_timesheet_detail/sales_order_timesheet_detail.py
@@ -66,7 +66,7 @@ def insert_so_time_log(tl_name):
         frappe.delete_doc('Sales Order Timesheet Detail', so_tl_name)
 
 def sort_so_time_logs(so):
-    so_tls = frappe.db.get_list('Sales Order Timesheet Detail', {'parent': so}, ['start_datetime', 'end_datetime', 'name'])
+    so_tls = frappe.db.get_all('Sales Order Timesheet Detail', {'parent': so}, ['start_datetime', 'end_datetime', 'name'])
     sso_tls = sorted(so_tls, key=lambda item: (item['start_datetime'], item['end_datetime']))
     n = 1
     for sso_tl in sso_tls:
