3b6666a63b21494bcd1323ee3f12957edece6ea4
marty <morez.martin@gmail.com>
2024-08-27 16:47:44 +0200

    try to solve problems in sotd

--- erpnext/projects/doctype/timesheet/timesheet.js
+++ erpnext/projects/doctype/timesheet/timesheet.js
@@ -5,7 +5,7 @@ frappe.ui.form.on("Timesheet", {
 	setup: function (frm) {
 		frappe.require("/assets/erpnext/js/projects/timer.js");
 
-		frm.ignore_doctypes_on_cancel_all = ["Sales Invoice", "Sales Order"];
+		frm.ignore_doctypes_on_cancel_all = ["Sales Invoice", "Sales Order", "Timesheet"];
 		frm.add_fetch('employee', 'employee_name', 'employee_name');
 		frm.fields_dict.employee.get_query = function () {
 			return {
